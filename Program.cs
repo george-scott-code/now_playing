d0b28bad6d9ea53d2df425b3f20ad0266b5283fe
george-scott-code <george.scott.code@gmail.com>
2023-07-04 20:04:08 +0100

    show album title
    🎧 02 - Hot Shit (Im Back!) | The Roots | Dilla Joints

--- Program.cs
+++ Program.cs
@@ -1,21 +1,24 @@
-﻿using Windows.Media.Control;
+﻿namespace now_playing;
+
+using Windows.Media.Control;
+
 internal class Program
 {
     public static async Task Main(string[] args)
     {
-        try 
+        try
         {
-            var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
-            var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
-            Console.WriteLine("{0} - {1}", mediaProperties.Artist, mediaProperties.Title);
+            var mediaControlSessionManager = await GetSystemMediaTransportControlsSessionManager();
+            var mediaProperties = await GetMediaProperties(mediaControlSessionManager.GetCurrentSession());
+            Console.WriteLine("{0} | {1} | {2}", mediaProperties.Title, mediaProperties.Artist, mediaProperties.AlbumTitle);
         }
-        catch 
+        catch
         {
-        }        
+            // none found
+        }
     }
-
+    
     private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() => await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
-
     private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session) =>
         await session.TryGetMediaPropertiesAsync();
 }
