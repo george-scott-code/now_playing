e9f5e1a7740ab9a94c90f72254b4c37e78d2d55b
george-scott-code <george.scott.code@gmail.com>
2023-07-04 19:45:07 +0100

    init
    🎧 Boiler Room - James Blake pres. Before | Boiler Room

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Media.Control;
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        try 
+        {
+            var gsmtcsm = await GetSystemMediaTransportControlsSessionManager();
+            var mediaProperties = await GetMediaProperties(gsmtcsm.GetCurrentSession());
+            Console.WriteLine("{0} - {1}", mediaProperties.Artist, mediaProperties.Title);
+        }
+        catch 
+        {
+        }        
+    }
+
+    private static async Task<GlobalSystemMediaTransportControlsSessionManager> GetSystemMediaTransportControlsSessionManager() => await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+
+    private static async Task<GlobalSystemMediaTransportControlsSessionMediaProperties> GetMediaProperties(GlobalSystemMediaTransportControlsSession session) =>
+        await session.TryGetMediaPropertiesAsync();
+}
