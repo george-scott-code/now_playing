c9138ab7f25ca43e7fb1065dfebc10ae8802c5f4
george-scott-code <george.scott.code@gmail.com>
2023-07-08 18:54:18 +0100

    script check output is not null
    🎧 Snail Mail: NPR Music Tiny Desk Concert | NPR Music |

--- gitnp.ps1
+++ gitnp.ps1
@@ -2,26 +2,33 @@ param(
     [string]$message
 )
 
-$programPath = "bin\Debug\net7.0-windows10.0.19041.0\now_playing.dll"
-$emojiIcon = [System.Convert]::toInt32("1F3A7", 16)
-$output = ([System.Char]::ConvertFromUtf32($emojiIcon)) + " "
+$programPath = "bin\Debug\net7.0-windows10.0.19041.0\now_playing.dll";
+$emojiIcon = [System.Convert]::toInt32("1F3A7", 16);
+$trackDetail = ([System.Char]::ConvertFromUtf32($emojiIcon)) + " ";
 
-$processInfo = New-Object System.Diagnostics.ProcessStartInfo
-$processInfo.FileName = "dotnet"
-$processInfo.Arguments = $programPath
-$processInfo.RedirectStandardOutput = $true
-$processInfo.UseShellExecute = $false
-$processInfo.CreateNoWindow = $true
+$processInfo = New-Object System.Diagnostics.ProcessStartInfo;
+$processInfo.FileName = "dotnet";
+$processInfo.Arguments = $programPath;
+$processInfo.RedirectStandardOutput = $true;
+$processInfo.UseShellExecute = $false;
+$processInfo.CreateNoWindow = $true;
 
-$process = New-Object System.Diagnostics.Process
-$process.StartInfo = $processInfo
+$process = New-Object System.Diagnostics.Process;
+$process.StartInfo = $processInfo;
 
-$process.Start() | Out-Null
-$process.WaitForExit()
+$process.Start() | Out-Null;
+$process.WaitForExit();
 
 if ($process.ExitCode -eq 0) {
-    $output += $process.StandardOutput.ReadToEnd()
+    $output = $process.StandardOutput.ReadToEnd()
+    if($output) {
+        $trackDetail += $output;
+        git commit -m $message -m $trackDetail;
+    }
+    else {
+        git commit -m $message;
+    }
 }
-
-git add .
-git commit -m $message -m $output
+else {
+    git commit -m $message;
+}
